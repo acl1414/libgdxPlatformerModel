d7c4bf6451abfdf8dd2210d728e6a7a9230ae334
alain clinquart <alain.clinquart@gmail.com>
2021-04-30 21:56:27 +0200

    First Commit Initialisation project

---
+++ desktop/src/com/acl14/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.acl14.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.acl14.game.GameLauncher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameLauncher(), config);
+	}
+}
